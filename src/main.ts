6026c9ecb8558e5a42bd72e5a69fae7ee3d16c1d
wangjian <wangjian0504@gmail.com>
2024-07-31 11:23:13 +0800

    feat: user module add

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /**
+   *  全局启用 ValidationPipe
+   *  transform: true 转换成 DTO 对象
+   */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
